aa92756923ed28c210bee691afa4fe1f5a2f3f8d
hardikhere <web.hardikhere@gmail.com>
2020-06-03 08:26:07 -0700

    added contact field

--- src/pages/Addlisting.js
+++ src/pages/Addlisting.js
@@ -8,6 +8,8 @@ const Addlist = ()=>{
      Address:"",
      imgUrl:"",
      url:"",
+     email:"",
+     phone:"",
      courses:[""],
      description:"",
      location:""
@@ -30,12 +32,15 @@ const Addlist = ()=>{
         Address:"",
         imgUrl:"",
         url:"",
+        email:"",
+        phone:"",
         courses:[""],
         description:"",
         location:""
      });
      setS(false);
  };
+ const {Address,imgUrl,url,email,phone,description,location} = data;
  const successMsg = ()=>{
      if(s) return <h1>Data saved</h1>;
      else return <h1>data is not saved yet</h1>
@@ -49,12 +54,15 @@ const Addlist = ()=>{
      <>
      <Base>
       {successMsg()}
-      <input placeholder="title" onChange={handlechange("title")} type="text" />
-      <input placeholder="Address" type="text" onChange={handlechange("Address")}  />
-      <input placeholder="imgUrl" type="text" onChange={handlechange("imgUrl")} />
-      <input placeholder="url" type="text" onChange={handlechange("url")} />
-      <input placeholder="description" type="text" onChange={handlechange("description")} />
-      <input placeholder="loaction" type="text" onChange={handlechange("location")} />
+      <input placeholder="title" value={title} onChange={handlechange("title")} type="text" />
+      <input placeholder="email" value={email} onChange={handlechange("email")} type="email" />
+      <input placeholder="phone" value={phone} onChange={handlechange("phone")} type="text" />
+
+      <input placeholder="Address"  value={Address} type="text" onChange={handlechange("Address")}  />
+      <input placeholder="imgUrl" value={imgUrl} type="text" onChange={handlechange("imgUrl")} />
+      <input placeholder="url" value={url}  type="text" onChange={handlechange("url")} />
+      <input placeholder="description" value={description} type="text" onChange={handlechange("description")} />
+      <input placeholder="loaction" value={location} type="text" onChange={handlechange("location")} />
       <input placeholder="courses" type="text" onChange={handlechange("courses")} />
        <button onClick={submit}>
            submit
